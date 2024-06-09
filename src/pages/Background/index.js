91ce2271323d3d4032b7af1dcb8d64c9463a4842
honeyclouddev <honeyclouddev@gmail.com>
2024-06-09 12:42:20 -0700

    Migrate the Tree tab manager to React

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,39 @@
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
+/*global chrome*/
+
+const NEW_TAB_URL = 'chrome://newtab/';
+
+chrome.tabs.onCreated.addListener((tab) => {
+  chrome.storage.session.get(['tabParentMap'], (ret) => {
+    let tabParentMap = ret.tabParentMap || {};
+    if (tab.url !== NEW_TAB_URL) {
+      tabParentMap[tab.id] = tab.openerTabId;
+    }
+    chrome.storage.session.set({ tabParentMap });
+  });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.url) {
+    if (tab.url === NEW_TAB_URL) {
+      chrome.storage.session.get(['tabParentMap'], (ret) => {
+        let tabParentMap = ret.tabParentMap || {};
+        if (tab.url === NEW_TAB_URL) {
+          delete tabParentMap[tab.id];
+        }
+        chrome.storage.session.set({ tabParentMap });
+      });
+    }
+  }
+});
+
+chrome.tabs.onRemoved.addListener((tabId) => {
+  chrome.storage.session.get(['tabParentMap'], (ret) => {
+    let tabParentMap = ret.tabParentMap || {};
+    delete tabParentMap[tabId];
+    chrome.storage.session.set({ tabParentMap });
+  });
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+});
--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import TabTree from '../Components/TabTree';
+import Initializer from '../Utils/Initializer';
 
-import Popup from './Popup';
+// import Popup from './Popup';
 import './index.css';
 
 const container = document.getElementById('app-container');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<Popup />);
+// root.render(<Popup />);
+
+root.render(
+    <TabTree
+        chrome={chrome}
+        initializer={new Initializer(chrome)}
+    />
+);

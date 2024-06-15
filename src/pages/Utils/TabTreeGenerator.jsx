f031bd26c0fa83c6f975fefbe1b402c919fff5ae
honeyclouddev <honeyclouddev@gmail.com>
2024-06-15 16:13:11 -0700

    Refactor TabTreeGenerator to use a functional component and hooks

--- src/pages/Utils/TabTreeGenerator.jsx
+++ src/pages/Utils/TabTreeGenerator.jsx
@@ -4,38 +4,41 @@ class TreeGenerator {
 
     constructor(tabs, tabParentMap) {
         this.tabs = tabs;
-        //TODO: solve how to clean tabMap later
-        // this.tabParentMap = this.cleanTabParentMap(tabs, tabParentMap)
         this.tabParentMap = tabParentMap;
         this.nodeMap = {};
         this.tabMap = {};
         this.rootNode = new TabTreeNode();
-        tabs.forEach((tab) => {
+        
+        // Create a map of tab IDs to tab objects
+        tabs.forEach(tab => {
             this.tabMap[tab.id] = tab;
         });
     }
 
+    // Generate the tree structure from tabs and parent map
     getTree() {
         this.tabs.forEach(tab => {
             let node = this.getNode(tab);
-            let parentNode = this.getNode(this.getParentTabId(tab.id));
+            let parentNode = this.getNode(this.getParentTab(tab.id));
             node.parent = parentNode;
             parentNode.children.push(node);
         });
         return this.rootNode;
     }
 
-    getParentTabId(tabId) {
+    // Get the parent tab object by tab ID
+    getParentTab(tabId) {
         let parentTabId = this.tabParentMap[tabId];
         if (this.tabMap[parentTabId]) {
             return this.tabMap[parentTabId];
-        } else if (!this.tabMap[parentTabId] && this.tabParentMap[parentTabId]) {
-            return this.getParentTabId(parentTabId);
+        } else if (parentTabId && this.tabParentMap[parentTabId]) {
+            return this.getParentTab(parentTabId);
         } else {
             return undefined;
         }
     }
 
+    // Get or create a node for the given tab
     getNode(tab) {
         if (tab === undefined) {
             return this.rootNode;
@@ -46,18 +49,20 @@ class TreeGenerator {
         return this.nodeMap[tab.id];
     }
 
+    // Clean the tab parent map by removing invalid entries
     cleanTabParentMap(tabs, tabParentMap) {
-        let currentTabMap = {};
-        tabs.forEach((tab) => {
-            currentTabMap[tab.id] = 1;
-        })
-        let ret = {};
-        for(let key in tabParentMap) {
+        const currentTabMap = tabs.reduce((map, tab) => {
+            map[tab.id] = true;
+            return map;
+        }, {});
+
+        let cleanedMap = {};
+        for (let key in tabParentMap) {
             if (currentTabMap[key]) {
-                ret[key] = tabParentMap[key];
+                cleanedMap[key] = tabParentMap[key];
             }
         }
-        return ret;
+        return cleanedMap;
     }
 }
 
